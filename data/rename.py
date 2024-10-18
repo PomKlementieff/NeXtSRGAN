fb8d6df495f35f41b1b8d51912008de3e9ff06ad
Sung-Wook Park <411050@scnu.ac.kr>
2024-10-18 23:01:24 +0900

    Create rename.py

---
+++ data/rename.py
@@ -0,0 +1,16 @@
+import os
+import glob
+
+def main():
+    folder = './data/KID_F/KID_F_train_LR_bicubic/X4'
+    DIV2K(folder)
+    print('Finished.')
+
+def DIV2K(path):
+    img_path_l = glob.glob(os.path.join(path, '*'))
+    for img_path in img_path_l:
+        new_path = img_path.replace('x2', '').replace('x3', '').replace('x4', '').replace('x8', '')
+        os.rename(img_path, new_path)
+
+if __name__ == "__main__":
+    main()
